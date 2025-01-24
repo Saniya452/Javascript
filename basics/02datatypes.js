b19932f405aef1d5c584e9c049c1ddf13ffb7172
Sania Ahmed <96288389+Saniya452@users.noreply.github.com>
2025-01-24 11:07:18 +0000

    datatypes

---
+++ basics/02datatypes.js
@@ -0,0 +1,66 @@
+" use strict "; //treat all JS code as newer version
+
+// alert("hello") //we are using node.js not browser
+
+// console.log(3 
+//     + 3);
+//     console.log("sania"); //code readability should be high
+   
+//DataTypes:
+let name = 'saniya'
+let age = 20
+let isVerified = true
+
+/* Primitive DataTypes: 
+// Primitive Data types are predefined. 
+// Primitive Data types will have certain values.
+// Size depends on the type of data structure.
+// Examples are numbers and strings.
+// It can start with a lowercase.
+
+Number = 20: "number"
+bigint = 2820493205803295905*: "bigint"
+string = "Sania": "string"
+boolean = true/false: "boolean"
+Null = Standalone Value: "object"	
+Undefined: "undefined"
+Symbol = Unique:	"symbol"
+*/
+
+/* Non-Primitive DataTypes:
+//  Non-Primitive data types are created by the programmer
+// 	Non-Primitive data types can be NULL.
+// 	Size is not fixed
+// 	Examples are Array and Linked List.
+//  It can start with uppercase.
+
+
+Object:
+typeof {name:'John'}   // Returns object
+typeof [1,2,3,4]       // Returns object
+typeof new Map()       // Returns object
+typeof new Set()       // Returns object
+
+Array:                       
+Basic Array Methods:      
+Array length
+Array toString()
+Array at()
+Array join()
+Array pop()
+Array push()
+
+Iteration Methods:	
+Array shift()
+Array unshift()
+Array delete()
+Array concat()
+Array copyWithin()
+Array flat()
+Array splice()
+Array toSpliced()
+Array slice()
+*/
+  
+
+	
